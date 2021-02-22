866ddf9bc9d0df85eed291f0dafb9c85321b0941
iravzdhanju <52969271+iravzdhanju@users.noreply.github.com>
2021-02-22 04:00:13 -0500

    updated the layout

--- src/Homescreen.js
+++ src/Homescreen.js
@@ -2,13 +2,28 @@ import React from "react";
 import Banner from "./Banner";
 import "./HomeScreen.css";
 import Nav from "./Nav";
+import requests from "./Requests";
+import Row from "./Row";
 function Homescreen() {
   return (
-    <div className="homescreen">
+    <div className='homeScreen'>
       {/* NAV */}
       <Nav />
       {/* Banner */}
       <Banner />
+
+      <Row
+        title='Netflix Originals'
+        fetchUrl={requests.fetchNetflixOriginals}
+        isLargeRow
+      />
+      <Row title='Top Rated' fetchUrl={requests.fetchTopRated} />
+      <Row title='Trending Now' fetchUrl={requests.fetchTrending} />
+      <Row title='Action Movies' fetchUrl={requests.fetchActionMovies} />
+      <Row title='Comedy Movies' fetchUrl={requests.fetchComedyMovies} />
+      <Row title='Horror Movies' fetchUrl={requests.fetchHorrorMovies} />
+      <Row title='Romance Movies' fetchUrl={requests.fetchRomanticMovies} />
+      <Row title='Documentaries' fetchUrl={requests.fetchDocumentaries} />
     </div>
   );
 }
