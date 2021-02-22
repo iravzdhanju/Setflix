2e0c64614cfa925d92ecee32afc9d418678cb599
iravzdhanju <52969271+iravzdhanju@users.noreply.github.com>
2021-02-22 00:37:29 -0500

    updating files

---
+++ src/Homescreen.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './HomeScreen.css';
+import Nav from './Nav';
+function Homescreen() {
+  return (
+    <div className='homescreen'>
+      {/* NAV */}
+      <Nav/>
+      {/* Banner */}
+    </div>
+  );
+}
+
+export default Homescreen;
---
+++ src/Nav.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Nav.css';
+function Nav() {
+  return (
+    <div className='nav'>
+      <h1>this is a nav</h1>
+      <img src="" alt=""/>
+    </div>
+  );
+}
+
+export default Nav;
