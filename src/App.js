a85dfbca19ec0ad648893480ca7ec19cb1ed86f2
iravzdhanju <52969271+iravzdhanju@users.noreply.github.com>
2021-02-22 09:13:29 -0500

    modifying the login method

--- src/App.js
+++ src/App.js
@@ -4,16 +4,27 @@ import Homescreen from "./screens/Homescreen";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import LoginScreen from "./screens/LoginScreen";
 import { auth } from "./firebase";
+import { useDispatch, useSelector } from "react-redux";
+import { login, logout, selectUser } from "./features/UserSlice";
 function App() {
-  const user = null;
+  const user = useSelector(selectUser);
+  const dispatch = useDispatch();
 
   useEffect(() => {
     const unsubscribe = auth.onAuthStateChanged((userAuth) => {
       if (userAuth) {
-        //Logged In
-        console.log(userAuth);
+        //sa
+        //as
+        ////Logged In
+        dispatch(
+          login({
+            uid: userAuth.uid,
+            email: userAuth.email,
+          })
+        );
       } else {
         // Logged out
+        dispatch(logout);
       }
     });
     return unsubscribe;
