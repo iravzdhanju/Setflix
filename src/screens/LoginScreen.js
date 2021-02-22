086b248692952d71bd06df48b08cf3f073ff2490
iravzdhanju <52969271+iravzdhanju@users.noreply.github.com>
2021-02-22 06:46:18 -0500

    updated the styles

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -14,6 +14,16 @@ function LoginScreen() {
         <div className='loginScreen_Body'>
           <h1>Unlimited films, TV Programmes and more.</h1>
           <h2>Watch anywhere, Cancel at any time</h2>
+          <h3>
+            Ready to Watch? Enter your email to create or restart your
+            membership.
+          </h3>
+          <div className='loginScreen_Input'>
+            <form>
+              <input type='email' placeholder='Email Address' />
+              <button className='loginScreen_getStarted'>GET STARTED</button>
+            </form>
+          </div>
         </div>
       </div>
     </div>
