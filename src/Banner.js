073cdec0374db77ff7aee040936716903648cf37
iravzdhanju <52969271+iravzdhanju@users.noreply.github.com>
2021-02-22 02:38:25 -0500

    added the api

--- src/Banner.js
+++ src/Banner.js
@@ -1,13 +1,16 @@
 import React from "react";
 import "./Banner.css";
 function Banner() {
+  function truncate(string, n) {
+    return string?.length > n ? string.substr(0, n - 1) + "..." : string;
+  }
   return (
     <div className="banner">
       <header
         className="banner"
         style={{
           backgroundSize: "cover",
-          backgroundImage: `url("https://deadline.com/wp-content/uploads/2020/07/netflix-logo.png")`,
+          backgroundImage: `url("https://www.fg-a.com/wallpapers/2020-black-crystalline-peaks-image.jpg")`,
           backgroundPosition: "center center",
         }}>
         <div className="banner_contents">
@@ -16,10 +19,14 @@ function Banner() {
             <button className="banner_button">Play</button>
             <button className="banner_button">My List</button>
           </div>
-          <div className="banner_description">This is a test Description</div>
+          <div className="banner_description">
+            {truncate(
+              "This is a test Description This is a test Description This is a test Description This is a test Description This is a test Description This is a test DescriptionThis is a test Description This is a test Description This is a test Description This is a test Description This is a test Description This is a test Description This is a test Description",
+              150
+            )}
+          </div>
         </div>
-<div className="banner_fadebutton">
-</div>
+        <div className="banner_fadebutton"></div>
       </header>
     </div>
   );
---
+++ src/Requests.js
@@ -0,0 +1,5 @@
+const API_KEY = "4a59d724bc92248afc8ceed631e05ff1";
+
+const requests = {
+
+}
