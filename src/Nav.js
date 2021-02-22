3b1d705b1d2f258968e45ee4f80e2082cb805130
iravzdhanju <52969271+iravzdhanju@users.noreply.github.com>
2021-02-22 00:54:41 -0500

    added logo

--- src/Nav.js
+++ src/Nav.js
@@ -3,8 +3,19 @@ import './Nav.css';
 function Nav() {
   return (
     <div className='nav'>
-      <h1>this is a nav</h1>
-      <img src="" alt=""/>
+      <div className='nav_Contents'>
+        <img
+          className='nav_logo'
+          src='https://logos-download.com/wp-content/uploads/2016/03/Netflix_logo.png'
+          alt=''
+        />
+
+        <img
+          className='nav_avatar'
+          src='https://mir-s3-cdn-cf.behance.net/project_modules/disp/84c20033850498.56ba69ac290ea.png'
+          alt=''
+        />
+      </div>
     </div>
   );
 }
