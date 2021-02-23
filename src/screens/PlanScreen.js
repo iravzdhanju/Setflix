ba1b35bf6e821b5d676ceba88bac4fdfbe2dd2bd
iravzdhanju <52969271+iravzdhanju@users.noreply.github.com>
2021-02-23 17:48:07 -0500

    setting up Payments

--- src/screens/PlanScreen.js
+++ src/screens/PlanScreen.js
@@ -11,11 +11,33 @@ function PlanScreen() {
         const products = {};
         querySnapshot.forEach(async (productDoc) => {
           products[productDoc.id] = productDoc.data();
-          const 
+          const priceSnap = await productDoc.refcollection("price").get();
+          priceSnap.docs.foreach((price) => {
+            products[productDoc.id].prices = {
+              pricesId: price.id,
+              priceData: price.data(),
+            };
+          });
         });
+        setProducts(products);
       });
   }, []);
-  return <div className='planScreen'></div>;
+  return (
+    <div className='planScreen'>
+      {Object.entries(products).map(([productId, productData]) => {
+        //logic to check that user subscription is active
+        return (
+          <div className='planScreen_plan'>
+            <div className='planScreen_info'>
+              <h5>{productData.name}</h5>
+              <h6>{productData.description}</h6>
+            </div>
+            <button>Subscribe</button>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
 
 export default PlanScreen;
